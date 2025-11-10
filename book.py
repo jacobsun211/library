bc083a1d59a343262679829dc40de64b19ea5fbf
Ariel_Izralevitch <your.email@example.com>
2025-11-10 09:35:06 +0200

    ariel

---
+++ book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, author, ISBN, is_available):
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.is_available = True
---
+++ user.py
@@ -0,0 +1,3 @@
+class User :
+    def __init__(self, name, id, borrowed_books):
+        self.name = name

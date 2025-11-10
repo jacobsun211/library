bf0a87c17b353bc1fa5fa9ac85e8164a52ab603a
Ariel_Izralevitch <your.email@example.com>
2025-11-10 09:52:49 +0200

    funf

---
+++ library.py
@@ -0,0 +1,25 @@
+class Library:
+    def __init__(self):
+        self.list_of_books = []
+        self.list_of_users  = []
+
+    def add_book(self,book):
+        self.list_of_books.append(book)
+
+
+    def add_user(self,user):
+        self.list_of_users.append(user)
+
+    def borrow_book(self,user_id, book_isbn):
+        pass
+
+
+    def return_book(self,user_id, book_isbn):
+        pass
+
+    def list_available_books(self):
+        pass
+
+
+    def search_book(self,author):
+        pass
--- user.py
+++ user.py
@@ -1,3 +1,8 @@
 class User :
     def __init__(self, name, id, borrowed_books):
         self.name = name
+        self.id = id
+        self.borrowed_books = []
+        
+
+

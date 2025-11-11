1f40d8746dafbb6c9263b93c8ecb9145e5310c79
jacob sun <jacobsun211@gmail.com>
2025-11-11 11:37:26 +0200

    commit before merge

--- menu.py
+++ menu.py
@@ -1,13 +1,12 @@
-from library import Library
-from library import Book
-from library import User
+from .library import Library
+from .library import Book
+from .library import User
 
 
 def menu():
     option = 0
     while option != '7':
-        option = input(
-            'pick an option\n1. Add Book\n2. Add User\n3. Borrow Book\n4. return a book\n5.see all available books\n6. search a book\n7. Save & Exit\n\n  ')
+        option = input('pick an option\n1. Add Book\n2. Add User\n3. Borrow Book\n4. return a book\n5.see all available books\n6. search a book\n7. Save & Exit\n\n  ')
         if option == '1':
             title = input('please enter the title of the book: ')
             author = input('please enter the author of the book: ')

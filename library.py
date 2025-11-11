d33d39b82551eee672ba793bdaeb07765edc0cfc
jacob sun <jacobsun211@gmail.com>
2025-11-11 11:16:54 +0200

    modified library to update borrowed_books of user

--- library.py
+++ library.py
@@ -32,27 +32,36 @@ class Library:
             if user['id'] == user_id:
                 with open('books.json','r') as f:
                     books = json.load(f)
-                for i,book in enumerate(books):
+                for k,book in enumerate(books):
                     if book['isbn'] == book_isbn and book['is_available']:
-                        user['borrowed_books'].append(book)
                         book['is_available'] = False
+                        user['borrowed_books'].append(book)
+
+                        with open("users.json", 'w') as f:
+                            json.dump(users,f)
                         with open("books.json", 'w') as f:
                             json.dump(books,f)
-                        print(f'{books[i]['title']} is borrowed')
+                        print(f'{books[k]['title']} is borrowed')
 
 
     @staticmethod
     def return_book(user_id, book_isbn):
         with open('users.json','r')as f:
             users = json.load(f)
+        with open('books.json', 'r') as f:
+            books = json.load(f)
         for user in users:
-            if user.id == user_id:
-                with open('books.json','r') as f:
-                    books = json.load(f)
-                for book in books:
-                    if book.isbn == book_isbn:
-                        user.borrowed_books.pop(book)
-                        book.is_available = True
+            if user['id'] == user_id:
+                for k,book in enumerate(books):
+                    if book['isbn'] == book_isbn:
+                        i = user['borrowed_books'].index(book)
+                        user['borrowed_books'].pop(i)
+
+                        with open('users.json', 'w') as f:
+                            json.dump(users,f)
+                        book['is_available'] = True
+                        with open('books.json', 'w') as f:
+                            json.dump(books,f)
 
     @staticmethod
     def list_available_books():
@@ -84,6 +93,7 @@ Library.add_book(book3)
 user = User('jacob')
 Library.add_user(user)
 Library.borrow_book(user.id,book2.isbn)
+Library.return_book(user.id,book2.isbn)
 Library.list_available_books()
 
 
